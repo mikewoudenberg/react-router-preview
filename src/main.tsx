232b41cb5aca3a4ccdb32e08a991c5908b3316ad
Mike Woudenberg <mwoudenberg@xebia.com>
2022-09-03 17:21:03 +0200

    Adds basic routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { DataBrowserRouter, Route } from "react-router-dom";
+import ErrorPage from "./error-page";
+import "./index.css";
+import Root from "./routes/root";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <DataBrowserRouter>
+      <Route path="/" element={<Root />} errorElement={<ErrorPage />} />
+    </DataBrowserRouter>
   </React.StrictMode>
-)
+);
