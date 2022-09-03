f55d8532d231074779f76f40141f8f7e319c3b92
Mike Woudenberg <mwoudenberg@xebia.com>
2022-09-03 17:26:19 +0200

    Adds Outlet and uses Link

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import { DataBrowserRouter, Route } from "react-router-dom";
 import ErrorPage from "./error-page";
 import "./index.css";
+import Contact from "./routes/contact";
 import Root from "./routes/root";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <DataBrowserRouter>
-      <Route path="/" element={<Root />} errorElement={<ErrorPage />} />
+      <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          errorElement={<ErrorPage />}
+        />
+      </Route>
     </DataBrowserRouter>
   </React.StrictMode>
 );
