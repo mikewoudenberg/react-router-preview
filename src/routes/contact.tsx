0eac6e35ca15410ca334207652214c95ac86cf26
Mike Woudenberg <mwoudenberg@xebia.com>
2022-09-06 10:50:40 +0200

    Adds example of deferred data fetching

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,5 +1,8 @@
+import { Suspense } from "react";
 import {
   ActionFunctionArgs,
+  Await,
+  defer,
   Form,
   LoaderFunctionArgs,
   useFetcher,
@@ -10,11 +13,9 @@ import { getContact, updateContact } from "../contacts";
 
 // TODO type refinement possible here?
 export async function loader({ params }: LoaderFunctionArgs) {
-  const contact = await getContact(params.contactId || "");
-  if (!contact) {
-    throw new Response("", { status: 404, statusText: "Not found" });
-  }
-  return contact;
+  const contactPromise = getContact(params.contactId || "");
+
+  return defer({ contactPromise });
 }
 
 export async function action({ params, request }: ActionFunctionArgs) {
@@ -27,54 +28,74 @@ export async function action({ params, request }: ActionFunctionArgs) {
 
 export default function Contact() {
   // TODO types break down here
-  const contact = useLoaderData() as ContactType;
+  const { contactPromise } = useLoaderData() as {
+    contactPromise: Promise<ContactType>;
+  };
 
   return (
-    <div id="contact">
-      <div>
-        <img key={contact.avatar} src={contact.avatar} />
-      </div>
+    <Suspense fallback={<h1>Fancy loading skeleton</h1>}>
+      <Await
+        resolve={contactPromise}
+        errorElement={<div>Contact failed to load</div>}
+      >
+        {(contact: ContactType) => (
+          <>
+            <div id="contact">
+              <div>
+                <img key={contact.avatar} src={contact.avatar} />
+              </div>
 
-      <div>
-        <h1>
-          {contact.first || contact.last ? (
-            <>
-              {contact.first} {contact.last}
-            </>
-          ) : (
-            <i>No Name</i>
-          )}{" "}
-          <Favorite contact={contact} />
-        </h1>
+              <div>
+                <h1>
+                  {contact.first || contact.last ? (
+                    <>
+                      {contact.first} {contact.last}
+                    </>
+                  ) : (
+                    <i>No Name</i>
+                  )}{" "}
+                  <Favorite contact={contact} />
+                </h1>
 
-        {contact.twitter && (
-          <p>
-            <a target="_blank" href={`https://twitter.com/${contact.twitter}`}>
-              {contact.twitter}
-            </a>
-          </p>
+                {contact.twitter && (
+                  <p>
+                    <a
+                      target="_blank"
+                      href={`https://twitter.com/${contact.twitter}`}
+                    >
+                      {contact.twitter}
+                    </a>
+                  </p>
+                )}
+
+                {contact.notes && <p>{contact.notes}</p>}
+
+                <div>
+                  <Form action="edit">
+                    <button type="submit">Edit</button>
+                  </Form>
+                  <Form
+                    method="post"
+                    action="destroy"
+                    onSubmit={(event) => {
+                      if (
+                        !confirm(
+                          "Please confirm you want to delete this record."
+                        )
+                      ) {
+                        event.preventDefault();
+                      }
+                    }}
+                  >
+                    <button type="submit">Delete</button>
+                  </Form>
+                </div>
+              </div>
+            </div>
+          </>
         )}
-
-        {contact.notes && <p>{contact.notes}</p>}
-
-        <div>
-          <Form action="edit">
-            <button type="submit">Edit</button>
-          </Form>
-          <Form
-            method="post"
-            action="destroy"
-            onSubmit={(event) => {
-              if (!confirm("Please confirm you want to delete this record.")) {
-                event.preventDefault();
-              }
-            }}
-          >
-            <button type="submit">Delete</button>
-          </Form>
-        </div>
-      </div>
-    </div>
+      </Await>
+    </Suspense>
   );
 }
 
