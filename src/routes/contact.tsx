66ec31be916bd802bbed2e808ebb01a7774497ae
Mike Woudenberg <mwoudenberg@xebia.com>
2022-09-06 11:08:21 +0200

    Uses component instead of Function as Child

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -5,6 +5,7 @@ import {
   defer,
   Form,
   LoaderFunctionArgs,
+  useAsyncValue,
   useFetcher,
   useLoaderData,
 } from "react-router-dom";
@@ -38,64 +39,61 @@ export default function Contact() {
         resolve={contactPromise}
         errorElement={<div>Contact failed to load</div>}
       >
-        {(contact: ContactType) => (
-          <>
-            <div id="contact">
-              <div>
-                <img key={contact.avatar} src={contact.avatar} />
-              </div>
-
-              <div>
-                <h1>
-                  {contact.first || contact.last ? (
-                    <>
-                      {contact.first} {contact.last}
-                    </>
-                  ) : (
-                    <i>No Name</i>
-                  )}{" "}
-                  <Favorite contact={contact} />
-                </h1>
-
-                {contact.twitter && (
-                  <p>
-                    <a
-                      target="_blank"
-                      href={`https://twitter.com/${contact.twitter}`}
-                    >
-                      {contact.twitter}
-                    </a>
-                  </p>
-                )}
-
-                {contact.notes && <p>{contact.notes}</p>}
-
-                <div>
-                  <Form action="edit">
-                    <button type="submit">Edit</button>
-                  </Form>
-                  <Form
-                    method="post"
-                    action="destroy"
-                    onSubmit={(event) => {
-                      if (
-                        !confirm(
-                          "Please confirm you want to delete this record."
-                        )
-                      ) {
-                        event.preventDefault();
-                      }
-                    }}
-                  >
-                    <button type="submit">Delete</button>
-                  </Form>
-                </div>
-              </div>
-            </div>
-          </>
-        )}
+        <ContactDetails />
       </Await>
     </Suspense>
+  );
+}
+
+function ContactDetails() {
+  const contact = useAsyncValue() as ContactType;
+
+  return (
+    <div id="contact">
+      <div>
+        <img key={contact.avatar} src={contact.avatar} />
+      </div>
+
+      <div>
+        <h1>
+          {contact.first || contact.last ? (
+            <>
+              {contact.first} {contact.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}{" "}
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact.twitter && (
+          <p>
+            <a target="_blank" href={`https://twitter.com/${contact.twitter}`}>
+              {contact.twitter}
+            </a>
+          </p>
+        )}
+
+        {contact.notes && <p>{contact.notes}</p>}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+          <Form
+            method="post"
+            action="destroy"
+            onSubmit={(event) => {
+              if (!confirm("Please confirm you want to delete this record.")) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
+        </div>
+      </div>
+    </div>
   );
 }
 
